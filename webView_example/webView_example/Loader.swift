72c054a1ea50ffab8739fd4084a3def8dbeecd6e
RodTeixeira <rodofikis@hotmail.com>
2023-09-03 01:43:11 -0300

    adjust

---
+++ webView_example/webView_example/Loader.swift
@@ -0,0 +1,43 @@
+//
+//  Loader.swift
+//  webView_example
+//
+//  Created by Rodolfo on 03/09/23.
+//
+
+import SwiftUI
+
+struct DynamicLoader: View {
+    
+    @State private var isLoading:Bool = false
+    @State private var color:Color = .pink
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+        ZStack{
+            Circle()
+                .trim(from: .zero,to: 0.6)
+                .stroke(LinearGradient(colors: [color,.black.opacity(0.1)], startPoint: .leading, endPoint: .trailing),style: StrokeStyle(lineWidth: 8,lineCap: .round))
+                .frame(width: 60,height: 60)
+                .rotationEffect(Angle(degrees: isLoading ? 360 : 0))
+                .animation(.linear(duration: 1).repeatForever(autoreverses: false), value: isLoading)
+        }
+        .onReceive(timer) { _ in
+            withAnimation(.easeInOut) {
+                isLoading = true
+                color = Color(red: Double.random(in: 0...1), green: Double.random(in: 0...1), blue: Double.random(in: 0...1))
+            }
+        }
+        .onAppear{
+            DispatchQueue.main.async {
+                isLoading = true
+            }
+        }
+    }
+}
+
+struct Loader_Previews: PreviewProvider {
+    static var previews: some View {
+        DynamicLoader()
+    }
+}
